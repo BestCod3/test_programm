6eef246dcac5e4cd13a7758e1752ede0eb1b3c13
ErbolKubatov <126507472+BestCod3@users.noreply.github.com>
2024-02-18 21:41:10 +0600

    create

--- lib/buttons/categoryButton.dart
+++ lib/buttons/categoryButton.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/material.dart';
 
+Color inactiveColor = Color.fromRGBO(248, 247, 245, 1);
+Color activeColor = Colors.blue;
+
 class CategoryButton extends StatelessWidget {
-  const CategoryButton({super.key, required this.text});
+  const CategoryButton(
+      {super.key,
+      required this.text,
+      required this.color,
+      required this.onTap});
   final String text;
+  final Color color;
+  final Function() onTap;
   @override
   Widget build(BuildContext context) {
     return InkWell(
+      onTap: onTap,
       child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Container(
           width: MediaQuery.of(context).size.width * 0.25,
           height: MediaQuery.of(context).size.height * 0.09,
           decoration: BoxDecoration(
-              color: const Color.fromRGBO(248, 247, 245, 1),
+              color: color,
+              // color: const Color.fromRGBO(248, 247, 245, 1),
               borderRadius: BorderRadius.circular(10)),
           child: Center(
             child: Text(
--- lib/widgets/my_horizontal_categories.dart
+++ lib/widgets/my_horizontal_categories.dart
@@ -2,10 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:test_programm/buttons/categoryButton.dart';
 import 'package:test_programm/view/AsianKitchen/asian_kitchen.dart';
 
-class MyHorizontalList extends StatelessWidget {
-  const MyHorizontalList({
+Color inactiveColor = Color.fromRGBO(248, 247, 245, 1);
+Color activeColor = Colors.blue;
+
+class MyHorizontalList extends StatefulWidget {
+  MyHorizontalList({
     super.key,
   });
+
+  @override
+  State<MyHorizontalList> createState() => _MyHorizontalListState();
+}
+
+class _MyHorizontalListState extends State<MyHorizontalList> {
+  Color white_color = inactiveColor;
+
+  Color blue_color = activeColor;
 
   @override
   Widget build(BuildContext context) {
@@ -15,14 +27,26 @@ class MyHorizontalList extends StatelessWidget {
         height: 60,
         child: ListView(
           scrollDirection: Axis.horizontal,
-          children: const <Widget>[
+          children: <Widget>[
             CategoryButton(
               text: "Все меню",
+              color: white_color,
+              onTap: () {
+                setState(() {});
+                white_color = activeColor;
+                blue_color = inactiveColor;
+              },
             ),
-            CategoryButton(text: "Салаты"),
-            CategoryButton(text: "С рисом"),
-            CategoryButton(text: "С рыбой"),
-            CategoryButton(text: "С рыбой"),
+            CategoryButton(
+              text: "Салаты",
+              color: white_color,
+              onTap: () {
+                setState(() {});
+              },
+            ),
+            CategoryButton(text: "С рисом", color: white_color, onTap: () {}),
+            CategoryButton(text: "С рыбой", color: white_color, onTap: () {}),
+            CategoryButton(text: "С рыбой", color: white_color, onTap: () {}),
           ],
         ),
       ),
